75398c06458b8e7adb40426a04ea69d5b2f81c6b
NJH <125264369+hollow-oak@users.noreply.github.com>
2026-02-19 02:31:47 +0800

    map basics

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Collections;
 
 public class GridManager : MonoBehaviour
 {
@@ -14,9 +15,17 @@ public class GridManager : MonoBehaviour
         pathGenerator = new PathGenerator(gridWidth, gridHeight);
         List<Vector2Int> pathCells = pathGenerator.GeneratePath();
 
+        StartCoroutine(LayPathCells(pathCells));
+    }
+
+    private IEnumerator LayPathCells(List<Vector2Int> pathCells)
+    {
         foreach (Vector2Int pathCell in pathCells)
         {
             Instantiate(pathTile, new Vector3(pathCell.x, 0f, pathCell.y), Quaternion.identity);
+            yield return new WaitForSeconds(0.5f);
         }
+
+        yield return null;
     }
 }
--- Assets/Scripts/PathGenerator.cs
+++ Assets/Scripts/PathGenerator.cs
@@ -15,14 +15,49 @@ public class PathGenerator : MonoBehaviour
     public List<Vector2Int> GeneratePath()
     {
         pathCells = new List<Vector2Int>();
-
+        int x = 0;
         int y = (int)(height / 2);
 
+        /*
         for (int x = 0; x < width; x++)
         {
             pathCells.Add(new Vector2Int(x, y));
         }
+        */
+
+        while (x < width)
+        {
+            pathCells.Add(new Vector2Int(x, y));
+
+            bool validMove = false;
+
+            while (!validMove)
+            {
+                int move = Random.Range(0, pathCells.Count);
+
+                if (move == 0 || x % 2 == 0)
+                {
+                    x++;
+                    validMove = true;
+                }
+                else if (move == 1 && CellIsFree(x, y + 1) && y < (height - 3))
+                {
+                    y++;
+                    validMove = true;
+                }
+                else if (move == 2 && CellIsFree(x, y -1) && y > 2)
+                {
+                    y--;
+                    validMove = true;
+                }
+            }
+        }
 
         return pathCells;
+    }
+
+    private bool CellIsFree(int x, int y)
+    {
+        return !pathCells.Contains(new Vector2Int(x, y));
     }
 }
